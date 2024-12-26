c72c31c8a4e1346dd58aed08bf18df0903b661d2
Rosa <costurezati@gmail.com>
2024-12-26 14:46:05 -0300

    .

---
+++ scripts/teste03.py
@@ -0,0 +1,4 @@
+data=input('Digite a data de nascimento:')
+mes=input('Digite o mês de nascimento:')
+ano=input('Digite o ano de nascimento:')
+print('Você nasceu no dia ' +data+ ' de '+mes+ ' de '+ano)
