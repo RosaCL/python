aea6d533c194557bba6eeb47319a515294522cbb
Rosa <costurezati@gmail.com>
2024-12-29 07:40:43 -0300

    .

--- exercicios/ex005.py
+++ exercicios/ex005.py
@@ -2,3 +2,6 @@ n=int(input('Digite um número: '))
 na=n-1
 ns=n+1
 print('O antecessor de {} é {} e o sucessor é {}.'.format(n,na,ns))
+
+n=int(input('Digite um número: '))
+print('O antecessor de {} é {} e o sucessor é {}.'.format(n,n-1,n+1))
---
+++ exercicios/ex006.py
@@ -0,0 +1,2 @@
+n=int(input('Digite um número:'))
+print('O valor {}, tem o dobro de {}, o triplo é {} e a raiz quadrada é {:.2f}'.format(n,(n*2),(n*3),(n**(1/2))))
