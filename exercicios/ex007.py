da59f5f5fcbec8bfd243b1b4c7b45fc3250f32bf
Rosa <costurezati@gmail.com>
2024-12-30 15:41:06 -0300

    .

--- exercicios/ex007.py
+++ exercicios/ex007.py
@@ -1,4 +1,5 @@
-n1=int(input('Digite a nota:'))
-n2=int(input('Digite a nota:'))
+n1=float(input('Digite sua nota:'))
+n2=float(input('Digite sua nota:'))
 
-print('A sua média é{}'.format((n1+n2)/2))
+
+print('A sua média é {:.1f}'.format((n1+n2)/2))
---
+++ exercicios/ex008.py
@@ -0,0 +1,4 @@
+m=float(input('Digite um valor em metros:'))
+print('{} metros tem: '.format(m))
+print('{} cm'.format(m*100))
+print('{} mm'.format(m*1000))
