6840f2c7bb08b10249ad62c12601c58db14cbe04
Rosa <costurezati@gmail.com>
2025-01-02 09:26:45 -0300

    .

---
+++ exercicios/ex010.py
@@ -0,0 +1,2 @@
+n=float(input('Digite o valor em real: '))
+print('R$ {:.2f} em dolar é {:.2f} e em  Libra Esterlina é {:.2f} '.format(n, n/6.1922994, n/7.7619985))
---
+++ exercicios/ex011.py
@@ -0,0 +1,3 @@
+l=float(input('A largura da parede é: '))
+a=float(input('A altura da parede é: '))
+print('A parede tem {} x {} e sua área é de {} m², você vai utilizar {} litros de tinta' .format(l,a, (l*a),(l*a)/2 ))
