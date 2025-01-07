44cd350eae7bf1bf9a6978bb531597b9260dfcdf
Rosa <costurezati@gmail.com>
2025-01-07 13:24:47 -0300

    .

--- exercicios/ex014.py
+++ exercicios/ex014.py
@@ -1,1 +1,5 @@
-t=float(input())
+t=float(input('Digite a temperatura em Celsius: '))
+print('A temperatura {:.2F} ºC  em Fahrenheit é {:.2F} ºF'.format(t,((t*9)/5)+32))
+
+tf=float(input('Digite a temperatura em Fahrenheit: '))
+print('A temperatura {:.2f} ºF  em Celsius é {:.2F} ºC'.format(tf,(tf-32)*5/9))
---
+++ exercicios/ex015.py
@@ -0,0 +1,3 @@
+d=int(input('Quantos dias o carro foi alugado?'))
+km=float(input('Quantos km foi rodado?'))
+print('O aluguel do carro ficou R$ {:.2f}'.format((d*60)+km*0.15))
