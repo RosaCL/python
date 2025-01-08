8fff2dc2ac072effa32e4b58a9608ef2ebe7bc27
Rosa <costurezati@gmail.com>
2025-01-08 10:48:51 -0300

    .

--- Módulos/fromimport.py
+++ Módulos/fromimport.py
@@ -2,3 +2,12 @@ from math import sqrt, floor
 n=int(input('Digite um número:'))
 raiz=sqrt(n)
 print('A raiz de {} é {:.2f}'.format(n,floor(raiz)))
+
+#número aleatórios
+import random
+n=random.random()
+print (n)
+import random
+n=random.randint(1,10)
+print (n)
+
