f4a6fe0f3dfefd0665d41f0531f8f7c4d9043ae1
Rosa <costurezati@gmail.com>
2025-01-08 10:33:59 -0300

    .

---
+++ Módulos/fromimport.py
@@ -0,0 +1,4 @@
+from math import sqrt, floor
+n=int(input('Digite um número:'))
+raiz=sqrt(n)
+print('A raiz de {} é {:.2f}'.format(n,floor(raiz)))
---
+++ Módulos/import.py
@@ -0,0 +1,6 @@
+import math
+n=int(input('Digite um número:'))
+raiz=math.sqrt(n)
+print('A raiz de {} é igual a {:.2f}.'.format(n,raiz))
+# print('A raiz de {} é igual a {}.'.format(n,math.ceil(raiz))) #arredondado para cima
+# print('A raiz de {} é igual a {}.'.format(n,math.floor(raiz))) #arredondado para baixo
