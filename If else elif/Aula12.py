247264490705be1b4a3c063de17923c21a766557
Rosa <costurezati@gmail.com>
2025-01-23 10:56:50 -0300

    .

---
+++ If else elif/Aula12.py
@@ -0,0 +1,19 @@
+#estrutura simples
+nome=str(input('Qual é o seu nome?'))
+if nome=='Rosa':
+    print('Que nome lindo!')
+#estrutura composta
+nome=str(input('Qual é o seu nome?'))
+if nome=='Rosa':
+    print('Que nome lindo!')
+else:
+    print('Que nome legal!')
+# estrutura condição aninhada
+nome=str(input('Qual é o seu nome?'))
+if nome=='Rosa':
+    print('Que nome lindo!')
+elif nome=='Maria' or nome=='Círia' or nome=='Ângela':
+    print('Seu nome é bem bonito')
+    
+else:
+    print('Que nome legal!')
