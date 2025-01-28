3f13db730ae4481f6e4964104052a3ba9cb6d4a5
Rosa <costurezati@gmail.com>
2025-01-28 08:10:45 -0300

    .

---
+++ If else elif/ex038.py
@@ -0,0 +1,8 @@
+n1=int(input('Digite um número inteiro:'))
+n2=int(input('Digite outro número inteiro:'))
+if n1>n2:
+    print('O primeiro valor é MAIOR')
+elif n1==n2:
+    print('Não existe valor MAIOR, os dois são IGUAIS')
+else:
+    print('O segundo valor é MAIOR')
---
+++ If else elif/ex039.py
@@ -0,0 +1,17 @@
+from datetime import date
+atual=date.today().year
+nasc=int(input('Ano de nascimento:'))
+idade=atual-nasc
+print('Quem nasceu em {} tem {} anos em {}.'.format(nasc, idade, atual))
+if idade==18:
+    print('Você tem que se alistar IMEDIATAMENTE!')
+elif idade<18:
+    saldo=18-idade
+    print("Você ainda não tem 18 anos. Ainda falta {} anos para o alistamento.".format(saldo))
+    ano=atual+saldo
+    print('Você iria se alistar em {} anos.'.format(ano))
+else:
+    saldo=idade-18
+    print(' Você já deveria ter se alistado há {} anos.'.format(saldo))
+    ano=atual-saldo
+    print('Você deveria ter se alistado há {} anos. '.format(ano))
