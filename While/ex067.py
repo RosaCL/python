d75f97b063afd29cd9e493d6a322d36d0c48c87d
Rosa <costurezati@gmail.com>
2025-02-27 07:19:14 -0300

    .

---
+++ While/ex067.py
@@ -0,0 +1,10 @@
+n=c=0
+while True:
+    n=int(input("Quer ver a tabuada de qual valor?"))
+    if n<0:
+        break
+    print('¨'*30)
+    for c in range(1,11):
+        print('{} x {} = {}'.format(n,c,(n*c)))
+    print('¨'*30)
+print('Beijinho, beijinho Xau Xau')
